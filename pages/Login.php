9db6ceebc168b132c6aa4ab9343a7fb796bcabf0
Imam Hanafi <imamh7052@gmail.com>
2020-12-04 19:02:06 +0700

    auth login

--- pages/Login.php
+++ pages/Login.php
@@ -16,30 +16,11 @@
               <div class="card-body">
                 <h1>Login</h1>
                 <p class="text-muted">Sign In to your account</p>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="icon-user"></i>
-                    </span>
-                  </div>
-                  <input class="form-control" type="text" placeholder="Username">
-                </div>
-                <div class="input-group mb-4">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">
-                      <i class="icon-lock"></i>
-                    </span>
-                  </div>
-                  <input class="form-control" type="password" placeholder="Password">
-                </div>
-                <div class="row">
-                  <div class="col-6">
-                    <button class="btn btn-primary px-4" type="button">Login</button>
-                  </div>
-                  <div class="col-6 text-right">
-                    <button class="btn btn-link px-0" type="button">Forgot password?</button>
-                  </div>
-                </div>
+                
+                <form method="POST" action="../controller.php">
+                  <input type="text" name="nama">
+                  <button type="submit" name="tambah" id="tambah">Tambah</button>
+                </form>
               </div>
             </div>
             <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
